da9aaf67446723b3fa7b23d7973c4654b3d63335
Amy Mok <amymok@codeforamerica.org>
2014-03-31 13:38:20 -0700

    Add example pre-defined responses

--- main.py
+++ main.py
@@ -4,15 +4,23 @@ import twilio.twiml
 
 app = Flask(__name__)
 
-
+answer_dict =	{
+								"Hi, How Are You": "Thanks for checking out Cheeky City Bot!",
+								"Where Am I": "Code for America Office!",
+								"Set my alarm": "Sorry, I'm not Siri"
+							};
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     """Respond to incoming calls with a simple text message."""
 
-    rcvmsg = request.values.get('Body', None)
+    rcv_msg = request.values.get('Body', None)
     resp = twilio.twiml.Response()
-    resp.message(rcvmsg)
+    resp_msg = "Sorry, I don't have any information!"
+    if answer_dict.has_key(rcv_msg) :
+    	resp_msg = dict[rcv_msg]
+
+    resp.message(resp_msg)
     return str(resp)
 
 if __name__ == "__main__":
