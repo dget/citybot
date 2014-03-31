96829db60d75a0e0b9a2290ab335387d40dc1817
Amy Mok <amymok@codeforamerica.org>
2014-03-31 13:23:25 -0700

    respond with received msg

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import twilio.twiml
 
 app = Flask(__name__)
 
+
+
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     """Respond to incoming calls with a simple text message."""
 
+    rcvmsg = request.values.get('Body', None)
     resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
+    resp.message(rcvmsg)
     return str(resp)
 
 if __name__ == "__main__":
