e11dc636a2b5bec916191ffbc9783fb86d657fd4
Sam Hashemi <sam@samhashemi.com>
2014-03-31 16:59:36 -0700

    Google maps integration

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 import twilio.twiml
 from requests import get
 from re import match
+import cgi
+import os
 
 app = Flask(__name__)
 
@@ -17,22 +19,29 @@ def respond_to_question():
     rcv_msg = request.values.get('Body', None).lower()
     if "where" in rcv_msg and "taco" in rcv_msg:
         location = match(r".+in\s+(.+)", rcv_msg).group(1)
-        key = "schmows"
-        resp_msg = go_find_taco(location, key)
+        resp_msg = go_find_taco(location)
+    else:
+        resp_msg = "Sorry, I don't have any information!"
+        if rcv_msg in answer_dict:
+            resp_msg = answer_dict[rcv_msg]
 
     resp = twilio.twiml.Response()
-    resp_msg = "Sorry, I don't have any information!"
-    if rcv_msg in answer_dict:
-        resp_msg = answer_dict[rcv_msg]
-
     resp.message(resp_msg)
     return str(resp)
 
-def go_find_taco(location, key):
-    geocode_url = u"https://maps.googleapis.com/maps/api/geocode/json?&address=%skey=%s&sensor=false" % (location, key)
+def go_find_taco(location):
+    location = cgi.escape(location)
+    geocode_url = u"https://maps.googleapis.com/maps/api/geocode/json?&address=%s&key=%s&sensor=false" % (location, os.environ["GOOGLE_MAPS_KEY"])
     resp_json = get(geocode_url).json()
-    return resp_json
 
+    coords = resp_json['results'][0]['geometry']['location']
+    lat = coords['lat']
+    lng = coords['lng']
+
+    # Get specific food options based on coordinates
+    # geocode_url
+
+    return resp_json
     
  
 if __name__ == "__main__":
