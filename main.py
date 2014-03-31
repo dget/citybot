484fa431e28ad95b32ce2e4cd7a394830529a91f
Erica Kwan <erica@codeforamerica.org>
2014-03-31 15:17:13 -0700

    Working on looking for tacos

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import twilio.twiml
+from requests import get
 
 app = Flask(__name__)
 
@@ -12,8 +13,11 @@ answer_dict =   {
 @app.route("/", methods=['GET'])
 def respond_to_question():
     """Respond to incoming calls with a simple text message."""
-
     rcv_msg = request.values.get('Body', None).lower()
+    if "where" in rcv_msg and "taco" in rcv_msg
+        location =
+        key = "schmows"
+        go_find_taco(, key)
     resp = twilio.twiml.Response()
     resp_msg = "Sorry, I don't have any information!"
     if rcv_msg in answer_dict:
@@ -21,6 +25,11 @@ def respond_to_question():
 
     resp.message(resp_msg)
     return str(resp)
+
+
+def go_find_taco(address, key):
+    geocode_url =
+u"https://maps.googleapis.com/maps/api/geocode/json?&address=%skey=%s&sensor=false" % (address, key)
 
 if __name__ == "__main__":
     app.run(debug=True)
