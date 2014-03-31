efe3bb214663405dbd79060d943a6f49d70fbc3f
Dan Getelman <dgetelman@gmail.com>
2014-03-31 11:58:37 -0700

    Respond Mobile Monkey

---
+++ main.py
@@ -0,0 +1,17 @@
+# Download the twilio-python library from http://twilio.com/docs/libraries
+from flask import Flask, request, redirect
+import twilio.twiml
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming calls with a simple text message."""
+
+    resp = twilio.twiml.Response()
+    resp.message("Hello, Mobile Monkey")
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
